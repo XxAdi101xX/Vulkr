531f54636846386ef40febf34408dee821edfba2
Adithya Venkatarao <adi_101@live.com>
2020-12-06 18:54:18 -0500

    Create sampler class

---
+++ src/core/sampler.h
@@ -0,0 +1,49 @@
+/* Copyright (c) 2020 Adithya Venkatarao
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#pragma once
+
+#include "common/vulkan_common.h"
+
+namespace vulkr
+{
+
+class Device;
+
+class Sampler
+{
+public:
+	Sampler(Device &device, const VkSamplerCreateInfo &samplerInfo);
+	~Sampler();
+
+	Sampler(Sampler &&) = delete;
+	Sampler(const Sampler &) = delete;
+	Sampler& operator=(const Sampler &) = delete;
+	Sampler& operator=(Sampler &&) = delete;
+
+	VkSampler getHandle() const;
+private:
+	Device& device;
+	VkSampler handle{ VK_NULL_HANDLE };
+};
+
+} // namespace vulkr
