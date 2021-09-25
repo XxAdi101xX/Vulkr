6a16f4b6b81027bc771060d70bbb90e9c3c3a5ac
Adithya Venkatarao <adi_101@live.com>
2021-09-24 20:03:49 -0400

    Add -g glslc option to expose debug information in shader code

--- src/shaders/build.bat
+++ src/shaders/build.bat
@@ -1,8 +1,8 @@
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 main.vert -o spv/main.vert.spv
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 default.frag -o spv/default.frag.spv
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 textured.frag -o spv/textured.frag.spv
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 main.vert -o spv/main.vert.spv -g
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 default.frag -o spv/default.frag.spv -g
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 textured.frag -o spv/textured.frag.spv -g
 
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rgen -o spv/raytrace.rgen.spv
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rmiss -o spv/raytrace.rmiss.spv
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytraceShadow.rmiss -o spv/raytraceShadow.rmiss.spv
-%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rchit -o spv/raytrace.rchit.spv
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rgen -o spv/raytrace.rgen.spv -g
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rmiss -o spv/raytrace.rmiss.spv -g
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytraceShadow.rmiss -o spv/raytraceShadow.rmiss.spv -g
+%VULKAN_SDK%/Bin/glslc.exe --target-env=vulkan1.2 raytrace.rchit -o spv/raytrace.rchit.spv -g
